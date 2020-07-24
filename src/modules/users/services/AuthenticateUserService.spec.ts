8099206226884bf30e833960290f38e8969e1ec1
Albert-dias <albert.uft@gmail.com>
2020-07-24 14:49:08 -0300

    create tests authenticate initials

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,18 +1,37 @@
-import AppError from '@shared/errors/AppError';
+// import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUserService from './AuthenticateUserService';
+import CreateUserService from './CreateUserService';
 
 describe('AuthenticateUser', () => {
   it('should be able authenticate', async () => {
     const fakeUserRepository = new FakeUsersRepository();
-    const authenticateUser = new AuthenticateUserService(fakeUserRepository);
+    const fakeHashProvider = new FakeHashProvider();
 
-    const user = await authenticateUser.execute({
+    const createUser = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    const user = await createUser.execute({
+      name: 'Jhon Doe',
       email: 'jhondoe@gmail.com',
       password: '123456',
     });
 
-    expect(user).toHaveProperty('id');
+    const response = await authenticateUser.execute({
+      email: 'jhondoe@gmail.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
   });
 });
--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -2,11 +2,16 @@ import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUserRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUserRepository);
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
 
     const user = await createUser.execute({
       name: 'Jhon Doe',
@@ -19,7 +24,11 @@ describe('CreateUser', () => {
 
   it('should not be able to create a new user with same email from another', async () => {
     const fakeUserRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUserRepository);
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
 
     await createUser.execute({
       name: 'Jhon Doe',
