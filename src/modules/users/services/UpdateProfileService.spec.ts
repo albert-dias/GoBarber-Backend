11eacbefa2fab6c1dc869198922cf31e3c2ffe9b
Albert-dias <albert.uft@gmail.com>
2020-09-21 22:12:19 -0300

    Initial update profile

---
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -0,0 +1,34 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
+import FakeUserRepository from '../repositories/fakes/FakeUsersRepository';
+import UpdateUserAvatarService from './UpdateUserAvatarService';
+
+let fakeUsersRepository: FakeUserRepository;
+let fakeStorageProvider: FakeStorageProvider;
+let updateUserAvatar: UpdateUserAvatarService;
+
+describe('UpdateProfile', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUserRepository();
+    fakeStorageProvider = new FakeStorageProvider();
+    updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+  });
+  it('should be able to update avatar', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.jpg',
+    });
+
+    expect(user.avatar).toBe('avatar.jpg');
+  });
+});
--- src/modules/users/services/UpdateProfileService.ts
+++ src/modules/users/services/UpdateProfileService.ts
@@ -1,11 +1,11 @@
 import { injectable, inject } from 'tsyringe';
 
 // import uploadConfig from '@config/upload';
-import AppError from '../../../shared/errors/AppError';
+// import AppError from '../../../shared/errors/AppError';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 
-import User from '../infra/typeorm/entities/User';
+// import User from '../infra/typeorm/entities/User';
 
 interface IRequest {
   user_id: string;
@@ -25,25 +25,7 @@ class UpdateProfile {
     private hashProvider: IHashProvider,
   ) {}
 
-  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
-    const user = await this.usersRepository.findById(user_id);
-
-    if (!user) {
-      throw new AppError('Only authenticated users can change avatar', 401);
-    }
-
-    if (user.avatar) {
-      await this.storageProvider.deleteFile(user.avatar);
-    }
-
-    const filename = await this.storageProvider.saveFile(avatarFilename);
-
-    user.avatar = filename;
-
-    await this.usersRepository.save(user);
-
-    return user;
-  }
+  public async execute({ user_id, name, email }: IRequest): Promise<void> {}
 }
 
 export default UpdateProfile;
