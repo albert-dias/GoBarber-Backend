5b19fd36693986e7fd2fdfd90238540fc908f6b0
Albert Dias <albert.uft@gmail.com>
2020-10-23 00:27:10 -0300

    create appointment

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -17,6 +17,7 @@ describe('CreateAppointment', () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
       provider_id: '1234',
+      user_id: '1234',
     });
 
     expect(appointment).toHaveProperty('id');
@@ -29,12 +30,14 @@ describe('CreateAppointment', () => {
     await createAppointment.execute({
       date: appointmentDate,
       provider_id: '1234',
+      user_id: '1234',
     });
 
     expect(
       createAppointment.execute({
         date: appointmentDate,
         provider_id: '1234',
+        user_id: '1234',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
